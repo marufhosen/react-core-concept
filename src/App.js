702430168ecb0fe3b5d78a21ae8b6dba9c95a883
Maruf Islam <maruf15-1591@diu.edu.bd>
2021-06-27 01:01:31 +0600

    some changes

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  //const actors = ['Berline', 'Professor', 'Denver', 'Tokyo'];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Display Content</p>
+        <Person name='Cristiano Ronldo' job="Footballer of Juventus FC"></Person>
+        <Person name='Leonel Messi' job="Footballer of FCB"></Person>
+        <Person name='Naymer Junior' job="Footballer of PSG"></Person>
+        <Person name='Sergio Ramos' job="Footballer of ..."></Person>
       </header>
     </div>
   );
 }
+
+// Dynamic Data pass By 'props'
+function Person(props){
+  return(
+    <div style={{border: '2px solid purple', width: '60%', margin: '10px', padding: '10px'}}>
+      <h3>Name: {props.name}</h3>
+      <p>Professon: {props.job}</p>
+    </div>
+  );
+}
+
 
 export default App;
